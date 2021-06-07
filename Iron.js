a9f433c27e136dcae593509acd6c37a6f36d77e3
darshnair1 <82486240+darshnair1@users.noreply.github.com>
2021-06-07 15:31:44 -0500

    Add files via upload

---
+++ Iron.js
@@ -0,0 +1,28 @@
+class Iron {
+    constructor(x, y, width, height) {
+      var options = {
+        'density':30,
+        'friction': 0.9,
+        'restitution':0.3
+      };
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    };
+    display(){
+      var pos = this.body.position;
+      var angle = this.body.angle;
+  
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      strokeWeight(3);
+      stroke('gray')
+      fill('black')
+      rectMode(CENTER)
+      rect(0, 0, this.width, this.height);
+      pop();
+    };
+  };
+  
